1597b60caa9b9786c6860f06e37f1bb7a78407e8
buiduc9918 <buiduc9918>
2026-06-09 04:36:04 +0700

    1

---
+++ src/UTIL/axios.customize.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  timeout: 1000,
+  headers: {'X-Custom-Header': 'foobar'}
+});
+export default instance;
+
+//instance.headers.common['Authorization'] = AuthToken;
+instance.interceptors.request.use(
+  function (config) {
+    // Do something before the request is sent
+    return config;
+  },
+  function (error) {
+    // Do something with the request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lies within the range of 2xx causes this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that fall outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+
+  
+);
